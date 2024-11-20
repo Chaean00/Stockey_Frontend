80e2ee8ab5bc153aa3289be52da0a8abe7bcd370
kwak <sal091625@gmail.com>
2024-11-20 16:06:58 +0900

    setting:/#18: install tailwindcss material

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+const withMT = require('@material-tailwind/react/utils/withMT');
+export default withMT({
   content: ['./src/**/*.{html,js,jsx,ts,tsx}', './index.html'],
   theme: {
     colors: {
@@ -26,4 +27,4 @@ export default {
     },
   },
   plugins: [],
-};
+});
