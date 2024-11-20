32d07eb96ef6a038a075ae679b7e1217eabd16a7
kwak <sal091625@gmail.com>
2024-11-20 15:06:29 +0900

    feat/#18: tailwindcss color collection

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,23 @@
 export default {
   content: ['./src/**/*.{html,js,jsx,ts,tsx}', './index.html'],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      white: '#ffffff',
+      gray: {
+        100: '#F2F4F6', //sidebar bg
+        200: '#E0E8F0', //sidebar selected
+        300: '#DDDDDD', //stroke
+        400: '#A1A4AF', //sidebar gray text
+      },
+      black: '#4C526B',
+      red: '#FF7A73',
+      blue: {
+        100: '#88AAFD',
+        200: '#0046FF',
+      },
+    },
     extend: {},
   },
   plugins: [],
