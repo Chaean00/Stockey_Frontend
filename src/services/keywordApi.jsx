2469e7df2f87cf56fb9ca586ed49339fcfba9d67
JeongTaek <jtkim965@naver.com>
2024-11-25 13:43:00 +0900

    Refactor : API 경로 수정

--- src/services/keywordApi.jsx
+++ src/services/keywordApi.jsx
@@ -3,13 +3,13 @@ import axios from 'axios';
 
 const keywordApi = {
   //특정 종목에 대한 키워드 랭킹 반환
-  getKeywordRankAboutStock: (stock_id) => axiosInstance.get(`/api/keywords/stocks/${stock_id}`),
+  getKeywordRankAboutStock: (stock_id) => axios.get(`/api/keywords/stocks/${stock_id}`),
 
-  getStockRankAboutKeyword: (keyword_id) => axiosInstance.get(`/api/keywords/${keyword_id}/stock-rankings`),
+  getStockRankAboutKeyword: (keyword_id) => axios.get(`/api/keywords/${keyword_id}/stock-rankings`),
 
-  getKeywordRank: () => axiosInstance.get(`/api/keywords/total-rankings`),
+  getKeywordRank: () => axios.get(`/api/keywords/total-rankings`),
 
-  searchKeywordByWord: (word) => axiosInstance.get(`/api/keywords/${word}`)
+  searchKeywordByWord: (word) => axios.get(`/api/keywords/${word}`)
 };
 
 export default keywordApi;
