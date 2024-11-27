55370a494601d02fa1aa03cbc45761cc83414010
kwak <sal091625@gmail.com>
2024-11-27 11:35:34 +0900

    refactor(#48): landingPage chart data css

--- src/pages/LandingPage/StockBox.jsx
+++ src/pages/LandingPage/StockBox.jsx
@@ -111,10 +111,12 @@ export default function StockBox() {
   return (
     <div className="text-black_default flex flex-col bg-white">
       {/** Header */}
-      <div className="flex justify-between items-center mb-3">
+      <div className="flex justify-between items-center mb-4">
         <div className="flex items-center gap-3">
           <div className="font-extrabold text-2xl">
+            <span className="text-3xl font-bold text-blue-200">[ </span>
             {stockInfo.stock_name}
+            <span className="text-3xl font-bold text-blue-200"> ]</span>
             <span className="text-gray-600 text-xl hidden lg:inline-block">에 대한 키워드 랭킹</span>
           </div>
           <LikeButton isLiked={isLiked} addLike={handleAddLike} removeLike={handleRemoveLike} />
@@ -130,40 +132,78 @@ export default function StockBox() {
       {/** 그리드 레이아웃 */}
       <div className="grid grid-cols-5 gap-1 border-2 rounded-xl">
         {/** 리스트 (1/4 차지) */}
-        <div className="col-span-1 p-4 flex flex-col justify-center gap-2">
+        <div className="col-span-1 p-4 flex flex-col justify-center gap-1">
+          <div className="font-semibold text-lg mb-4">{stockInfo.stock_name}에서 가장 많이 언급된</div>
           {keywordRank?.slice(0, 10).map((el, i) => (
-            <div key={i} className="flex justify-between hover:bg-gray-100 rounded-xl pl-6">
-              <div className="text-blue-200 py-2 w-1/3 font-semibold text-lg">{i + 1}</div>
-              <div className="py-2 w-2/3 font-semibold">{el.keyword}</div>
+            <div key={i} className="flex justify-between hover:bg-gray-100 rounded-xl pl-5">
+              <div className="text-blue-200 py-1 w-1/3 font-semibold text-lg">{i + 1}</div>
+              <div className="py-1 w-2/3 font-semibold">{el.keyword}</div>
             </div>
           ))}
         </div>
 
         {/** 차트 (3/4 차지) */}
         <div ref={chartContainerRef} className="col-span-4 lg:p-4">
-          <Tabs id="period-tabs" activeKey={period} onSelect={moveToStock} className="mb-3">
-            <Tab eventKey="D" title="일봉">
-              {chartDataLoaded ? (
-                <CandleChart chartData={chartData} width={chartSize.width * 0.95} height={450} />
-              ) : (
-                <div>차트 데이터를 로드 중입니다...</div>
-              )}
-            </Tab>
-            <Tab eventKey="W" title="주봉">
-              {chartDataLoaded ? (
-                <CandleChart chartData={chartData} width={chartSize.width * 0.95} height={450} />
-              ) : (
-                <div>차트 데이터를 로드 중입니다...</div>
-              )}
-            </Tab>
-            <Tab eventKey="M" title="월봉">
-              {chartDataLoaded ? (
-                <CandleChartSimple chartData={chartData} width={chartSize.width * 0.95} height={450} />
-              ) : (
-                <div>차트 데이터를 로드 중입니다...</div>
-              )}
-            </Tab>
-          </Tabs>
+          {/** chart */}
+          <div>
+            <Tabs id="period-tabs" activeKey={period} onSelect={moveToStock} className="mb-3 font-semibold">
+              <Tab eventKey="D" title="일봉">
+                {chartDataLoaded ? (
+                  <CandleChart chartData={chartData} width={chartSize.width * 0.98} height={450} />
+                ) : (
+                  <div>차트 데이터를 로드 중입니다...</div>
+                )}
+              </Tab>
+              <Tab eventKey="W" title="주봉">
+                {chartDataLoaded ? (
+                  <CandleChart chartData={chartData} width={chartSize.width * 0.98} height={450} />
+                ) : (
+                  <div>차트 데이터를 로드 중입니다...</div>
+                )}
+              </Tab>
+              <Tab eventKey="M" title="월봉">
+                {chartDataLoaded ? (
+                  <CandleChartSimple chartData={chartData} width={chartSize.width * 0.98} height={450} />
+                ) : (
+                  <div>차트 데이터를 로드 중입니다...</div>
+                )}
+              </Tab>
+            </Tabs>
+          </div>
+
+          {/** chart data */}
+          <div className="mt-4 bg-gray-100 p-4 rounded-lg">
+            {chartData.length > 0 ? (
+              <div>
+                <ul className="flex flex-wrap items-center justify-between text-sm font-semibold">
+                  <li className="flex items-center gap-5 border-l-2 border-gray-300 pl-4">
+                    <span className="text-gray-500">날짜</span>
+                    <span>{chartData[chartData.length - 1]?.date || 'N/A'}</span>
+                  </li>
+                  <li className="flex items-center gap-5 border-l-2 border-gray-300 pl-4">
+                    <span className="text-gray-500">시가</span>
+                    <span>{chartData[chartData.length - 1]?.open || 'N/A'}</span>
+                  </li>
+                  <li className="flex items-center gap-5 border-l-2 border-gray-300 pl-4">
+                    <span className="text-gray-500">고가</span>
+                    <span>{chartData[chartData.length - 1]?.high || 'N/A'}</span>
+                  </li>
+                  <li className="flex items-center gap-5 border-l-2 border-gray-300 pl-4">
+                    <span className="text-gray-500">저가</span>
+                    <span>{chartData[chartData.length - 1]?.low || 'N/A'}</span>
+                  </li>
+                  <li className="flex items-center gap-5 border-l-2 border-gray-300 pl-4">
+                    <span className="text-gray-500">종가</span>
+                    <span>{chartData[chartData.length - 1]?.close || 'N/A'}</span>
+                  </li>
+                  <li className="flex items-center gap-5 border-l-2 border-gray-300 pl-4">
+                    <span className="text-gray-500">거래량</span>
+                    <span>{chartData[chartData.length - 1]?.volume || 'N/A'}</span>
+                  </li>
+                </ul>
+              </div>
+            ) : null}
+          </div>
         </div>
       </div>
     </div>
