964ffdd4dcbb7c6bbff6ba4aa032addfc562b601
kwak <sal091625@gmail.com>
2024-12-02 15:22:27 +0900

    refactor(#85):알림 설정 css

--- src/components/Alarm/Alarm.jsx
+++ src/components/Alarm/Alarm.jsx
@@ -106,45 +106,42 @@ function Alarm({ show, handleClose }) {
   return (
     <>
       <Modal show={show} onHide={handleClose}>
-        <Modal.Body className="font-sans">
+        <Modal.Body className="font-sans p-4 flex-col">
           <div className="flex justify-end">
             <CloseButton onClick={handleClose} />
           </div>
 
-          <div className="flex justify-between">
-            <h3 className="text-2xl font-bold">나만의 키워드/종목 알림 받기</h3>
-            <div className="flex justify-between">
-              <Form>
-                <Form.Check
-                  type="switch"
-                  id="custom-switch"
-                  checked={isSwitchOn} // 스위치 상태에 따라 checked 상태 변경
-                  onChange={handleSwitchChange} // 스위치 토글시 상태 변경
-                ></Form.Check>
-              </Form>
-            </div>
+          <div className="flex mb-3 items-center gap-2">
+            <h3 className="text-2xl font-extrabold">나만의 키워드/종목 알림 받기</h3>
+            <Form>
+              <Form.Check
+                type="switch"
+                className="text-blue-200 text-2xl"
+                checked={isSwitchOn} // 스위치 상태에 따라 checked 상태 변경
+                onChange={handleSwitchChange} // 스위치 토글시 상태 변경
+              ></Form.Check>
+            </Form>
           </div>
-          <div className="text-lg">
-            이 설정을 켜면 <span className="font-semibold">즐겨찾기한 키워드</span>와{' '}
-            <span className="font-semibold">종목</span>에 대한 <span className="font-semibold">순위 알림</span>을 받을
-            수 있어요
+          <div className="text-md py-2 border-b-2">
+            <span className="font-semibold">즐겨찾기한 키워드</span>와 <span className="font-semibold">종목</span>에
+            대한 <span className="font-semibold">순위 알림</span>을 받을 수 있어요
           </div>
 
           {/* isSwitchOn이 true일 때만 아래 내용 표시 */}
           {isSwitchOn && (
             <>
-              <div className="text-lg font-semibold mt-[15px]">
+              <div className="text-md font-semibold mt-4">
                 <a
                   href="https://join.slack.com/t/team-stockey/shared_invite/zt-2uj3dnj2u-JwP4~_dzpDr1yT~MAS59_A"
                   target="_blank" // 새 탭에서 링크 열기
                   rel="noopener noreferrer" // 보안상 권장되는 속성
-                  className="text-blue-500 hover:underline"
+                  className="text-blue-200 hover:underline"
                 >
                   링크 열기
                 </a>
                 를 클릭하여 stockey 슬랙에 참여한 후, 참여한 슬랙 계정을 입력해주세요
               </div>
-              <div className="mt-[7px]">
+              <div className="mt-2">
                 <Form>
                   <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
                     <Form.Control
@@ -153,18 +150,20 @@ function Alarm({ show, handleClose }) {
                       value={slackId}
                       onChange={(e) => setSlackId(e.target.value)}
                       autoFocus
-                      className="bg-[#F2F4F6] border-0 rounded-[8px]"
+                      className="bg-gray-100 border-0  rounded-md"
                     />
                   </Form.Group>
                 </Form>
               </div>
-              <div className="text-lg font-semibold">알림을 받을 키워드를 선택해주세요</div>
+              <div className="text-md font-semibold mt-4">
+                알림을 받을 <span className="text-blue-200">키워드</span>를 선택해주세요
+              </div>
               <div>
                 {keywordList?.length > 0 ? (
                   keywordList?.map((item, index) => (
                     <div
                       key={index}
-                      className={`inline-block px-2 py-1 m-1 rounded-[8px] ${item.alarm_status == 1 ? 'bg-[#E0E8F0]' : 'bg-[#F2F4F6]'} active:bg-[#E0E8F0] hover:cursor-pointer`}
+                      className={`inline-block px-2 py-1 m-1 rounded-md font-semibold text-sm ${item.alarm_status == true ? 'bg-blue-100' : 'bg-gray-100'} active:bg-blue-100  hover:cursor-pointer`}
                       onClick={() => {
                         keywordList[index].alarm_status = keywordList[index].alarm_status == 1 ? 0 : 1;
                         setKeywordList([...keywordList]);
@@ -174,19 +173,19 @@ function Alarm({ show, handleClose }) {
                     </div>
                   ))
                 ) : (
-                  <div className="py-3 flex flex-col justify-center items-center font-semibold">
-                    <h2 className="mb-1 text-gray-700">즐겨찾기한 키워드가 없습니다</h2>
+                  <div className="py-5 flex flex-col justify-center items-center font-semibold">
+                    <h2 className=" text-gray-800">즐겨찾기한 키워드가 없습니다</h2>
                     <p className="text-sm text-blue-200">관심 있는 키워드를 즐겨찾기에 추가해보세요</p>
                   </div>
                 )}
               </div>
-              <div className="text-lg font-semibold">알림을 받을 종목을 선택해주세요</div>
+              <div className="text-md font-semibold mt-4">알림을 받을 종목을 선택해주세요</div>
               <div>
                 {stockList?.length > 0 ? (
                   stockList?.map((item, index) => (
                     <div
                       key={index}
-                      className={`inline-block px-2 py-1 m-1 rounded-[8px] ${item.alarm_status == true ? 'bg-[#E0E8F0]' : 'bg-[#F2F4F6]'} active:bg-[#E0E8F0] hover:cursor-pointer`}
+                      className={`inline-block px-2 py-1 m-1 rounded-md font-semibold text-sm ${item.alarm_status == true ? 'bg-blue-100' : 'bg-gray-100'} active:bg-blue-100  hover:cursor-pointer`}
                       onClick={() => {
                         stockList[index].alarm_status = stockList[index].alarm_status == true ? false : true;
                         setStockList([...stockList]);
@@ -196,8 +195,8 @@ function Alarm({ show, handleClose }) {
                     </div>
                   ))
                 ) : (
-                  <div className="py-3 flex flex-col justify-center items-center font-semibold">
-                    <h2 className="mb-1 text-gray-700">즐겨찾기한 종목이 없습니다</h2>
+                  <div className="py-5 flex flex-col justify-center items-center font-semibold">
+                    <h2 className=" text-gray-800">즐겨찾기한 종목이 없습니다</h2>
                     <p className="text-sm text-blue-200">관심 있는 종목을 즐겨찾기에 추가해보세요</p>
                   </div>
                 )}
@@ -210,6 +209,7 @@ function Alarm({ show, handleClose }) {
               onClick={() => {
                 saveAlarm();
               }}
+              className="hover:bg-blue-100 bg-blue-200 hover:border-none"
             >
               저장
             </Button>
--- src/components/SidebarChat.jsx
+++ src/components/SidebarChat.jsx
@@ -1,148 +1,154 @@
-import { useChatContext } from "../utils/chatContext";
-import { useNavigate } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { useChatContext } from '../utils/chatContext';
+import { useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
-import chatApi from "../services/chatApi";
+import chatApi from '../services/chatApi';
 
 // Trending keywords data
 const trendingKeywords = [
-    { id: 1, title: "반도체" },
-    { id: 2, title: "이차전지" },
-    { id: 3, title: "트럼프" },
-  ]
+  { id: 1, title: '반도체' },
+  { id: 2, title: '이차전지' },
+  { id: 3, title: '트럼프' },
+];
 
-  // National keywords data with counts
-  const nationalKeywords = [
-    { id: 1, title: "금리", count: 356 },
-    { id: 2, title: "실적", count: 312 },
-    { id: 3, title: "배당", count: 216 },
-    { id: 4, title: "상승장", count: 195 },
-    { id: 5, title: "매출", count: 184 },
-    { id: 6, title: "인수합병", count: 120 },
-    { id: 7, title: "주식분할", count: 130 },
-    { id: 8, title: "경기침체", count: 65 },
-    { id: 9, title: "반도체", count: 65 },
-    { id: 10, title: "전기차", count: 65 },
-  ]
+// National keywords data with counts
+const nationalKeywords = [
+  { id: 1, title: '금리', count: 356 },
+  { id: 2, title: '실적', count: 312 },
+  { id: 3, title: '배당', count: 216 },
+  { id: 4, title: '상승장', count: 195 },
+  { id: 5, title: '매출', count: 184 },
+  { id: 6, title: '인수합병', count: 120 },
+  { id: 7, title: '주식분할', count: 130 },
+  { id: 8, title: '경기침체', count: 65 },
+  { id: 9, title: '반도체', count: 65 },
+  { id: 10, title: '전기차', count: 65 },
+];
 
 export default function SidebarChat() {
-    const { chatRoomList, setRoomId } = useChatContext();
-    const [ weightRoomList, setWeightRoomList ] = useState([]);
-    const [ bookmarkRoomList, setbookmarkRoomList ] = useState([]);
-    const navigate = useNavigate();
+  const { chatRoomList, setRoomId } = useChatContext();
+  const [weightRoomList, setWeightRoomList] = useState([]);
+  const [bookmarkRoomList, setbookmarkRoomList] = useState([]);
+  const navigate = useNavigate();
 
-    // 사이드 바에 순위로 표시된 채팅방 클릭 시
-    const roomListHandler = (roomId) => {
-        setRoomId(roomId);
-        navigate("/chat");
+  // 사이드 바에 순위로 표시된 채팅방 클릭 시
+  const roomListHandler = (roomId) => {
+    setRoomId(roomId);
+    navigate('/chat');
+  };
+
+  // 채팅방 룸 리스트 가져오기
+  useEffect(() => {
+    const fetchRoomList = async () => {
+      try {
+        // 가중치 랭킹
+        const weightResp = await chatApi.getWeightRankings();
+        setWeightRoomList(weightResp.data.results);
+
+        // 즐겨찾기 랭킹
+        const bookmarkResp = await chatApi.getBookmarkRankings();
+        setbookmarkRoomList(bookmarkResp.data.results);
+      } catch (error) {
+        console.error('룸 리스트 로드 실패:', error);
+      }
     };
+    fetchRoomList();
+  }, []);
 
-    // 채팅방 룸 리스트 가져오기
-    useEffect(() => {
-        const fetchRoomList = async () => {
-            try {
-                // 가중치 랭킹
-                const weightResp = await chatApi.getWeightRankings();
-                setWeightRoomList(weightResp.data.results);
-    
-                // 즐겨찾기 랭킹
-                const bookmarkResp = await chatApi.getBookmarkRankings();
-                setbookmarkRoomList(bookmarkResp.data.results);
-    
-            } catch (error) {
-                console.error('룸 리스트 로드 실패:', error);
-            }
-        }
-        fetchRoomList();
-    }, []);
+  return (
+    // <div className="p-4 text-defaultText">
+    //     {/** 전체 커뮤니티 */}
+    //     <div className="lg:text-2xl sm:text-lg mb-5 font-extrabold w-full border-b border-gray-300 pb-10">
+    //         <h3>전체 커뮤니티</h3>
+    //     </div>
 
-    return (
-        // <div className="p-4 text-defaultText">
-        //     {/** 전체 커뮤니티 */}
-        //     <div className="lg:text-2xl sm:text-lg mb-5 font-extrabold w-full border-b border-gray-300 pb-10">     
-        //         <h3>전체 커뮤니티</h3>
-        //     </div>
+    //     {/** 지금 뜨는 키워드 커뮤니티 */}
+    //     <div className="lg:text-2xl sm:text-lg mb-5 font-extrabold w-full border-b border-gray-300 pb-10">
+    //         <h3>지금 뜨는 키워드 커뮤니티</h3>
+    //     </div>
 
-        //     {/** 지금 뜨는 키워드 커뮤니티 */}
-        //     <div className="lg:text-2xl sm:text-lg mb-5 font-extrabold w-full border-b border-gray-300 pb-10">     
-        //         <h3>지금 뜨는 키워드 커뮤니티</h3>
-        //     </div>
+    //     {/** 국밥 키워드 커뮤니티 */}
+    //     <div className="lg:text-2xl sm:text-lg mb-5 font-extrabold w-full py-3">
+    //         <h3>국밥 키워드 커뮤니티</h3>
+    //         {/** list */}
+    // <ul className="flex flex-col gap-1 py-3">
+    //     {chatRoomList?.map((el, i) => {
+    //     return (
+    //         <li key={i} className="text-xl font-medium flex items-center hover:bg-gray-200 p-2 px-3 rounded-2xl" onClick={() => {
+    //             roomListHandler(el.id);
+    //         }}>
+    //         <div className="text-2xl text-blue-200 mr-16">{i + 1}</div>
+    //         <div>{el.name}</div>
+    //         </li>
+    //     );
+    //     })}
+    // </ul>
+    //     </div>
+    // </div>
+    <div className="w-full p-6">
+      {/* Title */}
+      <h2
+        className="text-lg font-extrabold text-gray-900 mb-8"
+        onClick={() => {
+          roomListHandler(1);
+        }}
+      >
+        전체 커뮤니티
+      </h2>
 
-        //     {/** 국밥 키워드 커뮤니티 */}
-        //     <div className="lg:text-2xl sm:text-lg mb-5 font-extrabold w-full py-3">     
-        //         <h3>국밥 키워드 커뮤니티</h3>
-        //         {/** list */}
-                // <ul className="flex flex-col gap-1 py-3">
-                //     {chatRoomList?.map((el, i) => {
-                //     return (
-                //         <li key={i} className="text-xl font-medium flex items-center hover:bg-gray-200 p-2 px-3 rounded-2xl" onClick={() => {
-                //             roomListHandler(el.id);
-                //         }}>
-                //         <div className="text-2xl text-blue-200 mr-16">{i + 1}</div>
-                //         <div>{el.name}</div>
-                //         </li>
-                //     );
-                //     })}
-                // </ul>
-        //     </div>
-        // </div>
-        <div className="w-full p-6">
-            {/* Title */}
-            <h2 className="text-lg font-semibold text-gray-900 mb-8" onClick={() => {
-                roomListHandler(1);
-            }}>
-                전체 커뮤니티
-            </h2>
+      {/* Divider */}
+      <div className="border-t border-gray-300 my-8" />
 
-            {/* Divider */}
-            <div className="border-t border-gray-300 my-8" />
+      {/* Trending Keywords Section */}
+      <div className="mb-8">
+        <h2 className="text-lg font-semibold text-gray-900 mb-4">지금 뜨는 키워드 커뮤니티</h2>
+        <ul>
+          {weightRoomList?.map((el, i) => {
+            return (
+              <li
+                key={i}
+                className="font-semibold flex items-center justify-between p-2 rounded-xl hover:bg-gray-200 text-md"
+                onClick={() => {
+                  roomListHandler(el.room_id);
+                }}
+              >
+                <div className="flex items-center">
+                  <div className="text-blue-200 w-12">{i + 1}</div>
+                  <div>{el.keyword}</div>
+                </div>
+                <span className="text-gray-400">???</span>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
 
-            {/* Trending Keywords Section */}
-            <div className="mb-8">
-                <h2 className="text-lg font-semibold text-gray-900 mb-4">
-                    지금 뜨는 키워드 커뮤니티
-                </h2>
-                <ul>
-                    {weightRoomList?.map((el, i) => {
-                    return (
-                        <li key={i} className="flex items-center justify-between p-2 rounded-xl hover:bg-gray-200" onClick={() => {
-                            roomListHandler(el.room_id);
-                        }}>
-                        <div className="flex items-center font-medium">
-                            <div className="text-blue-200 w-12">{i + 1}</div>
-                            <div>{el.keyword}</div>
-                        </div>
-                        <span className="text-gray-400 font-medium">???</span>
-                        </li>
-                    );
-                    })}
-                </ul>
-            </div>
+      {/* Divider */}
+      <div className="border-t border-gray-300 my-8" />
 
-            {/* Divider */}
-            <div className="border-t border-gray-300 my-8" />
-
-            {/* National Keywords Section */}
-            <div>
-                <h2 className="text-lg font-semibold text-gray-900 mb-2">
-                국밥 키워드 커뮤니티
-                </h2>
-                <ul>
-                    {bookmarkRoomList?.map((el, i) => {
-                    return (
-                        <li key={i} className="flex items-center justify-between p-2 rounded-xl hover:bg-gray-200" onClick={() => {
-                            roomListHandler(el.room_id);
-                        }}>
-                        <div className="flex items-center font-medium">
-                            <div className="text-blue-200 w-12">{i + 1}</div>
-                            <div>{el.keyword}</div>
-                        </div>
-                        <span className="text-gray-400 font-medium">{el.count}</span>
-                        </li>
-                    );
-                    })}
-                </ul>
-            </div>
-        </div>
-    );
+      {/* National Keywords Section */}
+      <div>
+        <h2 className="text-lg font-semibold text-gray-900 mb-2">국밥 키워드 커뮤니티</h2>
+        <ul>
+          {bookmarkRoomList?.map((el, i) => {
+            return (
+              <li
+                key={i}
+                className="flex items-center justify-between p-2 rounded-xl hover:bg-gray-200 font-semibold"
+                onClick={() => {
+                  roomListHandler(el.room_id);
+                }}
+              >
+                <div className="flex items-center ">
+                  <div className="text-blue-200 w-12">{i + 1}</div>
+                  <div>{el.keyword}</div>
+                </div>
+                <span className="text-gray-400 ">{el.count}</span>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
 }
