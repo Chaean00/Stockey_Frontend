8b73387326c0786e4e79b289f1e570cc63a3d28e
Pasak22 <125808024+Pasak22@users.noreply.github.com>
2024-12-03 01:20:53 +0900

    Feat(#94): 뉴스 박스 스크롤 구현

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import KeywordBox from './KeywordBox';
 import StockBox from './StockBox';
+import NewsBox from './NewsBox';
 
 export default function LandingPage() {
   return (
@@ -13,6 +14,11 @@ export default function LandingPage() {
       {/**종목에 대한 랭킹 및 차트 */}
       <div className="w-full ">
         <StockBox />
+      </div>
+
+      {/* '키워드 + 종목' 뉴스 카드 */}
+      <div className="w-full ">
+        <NewsBox />
       </div>
     </div>
   );
