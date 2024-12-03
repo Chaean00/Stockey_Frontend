727d8584a9bb262c4b6eaf7ac59432884939dbd0
Pasak22 <125808024+Pasak22@users.noreply.github.com>
2024-12-03 17:42:00 +0900

    Fix(#129): 채팅 메시지에 대한 시간을 서버에서 생성하도록 수정

--- vite.config.js
+++ vite.config.js
@@ -13,3 +13,18 @@ export default defineConfig({
     },
   },
 });
+
+// 로컬 테스트용
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     host: '0.0.0.0', // 모든 네트워크 인터페이스에 바인딩
+//     port: 3000, // 원하는 포트 (기본값: 5173)
+//     proxy: {
+//       '/api': {
+//         target: 'http://localhost:3000',
+//         changeOrigin: true,
+//       },
+//     },
+//   },
+// });
